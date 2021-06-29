deb792f9d70be3e567e2f3c01cbd423b2cf0ded8
unknown <okwuosahpashal@gmail.com>
2021-06-29 11:46:47 +0100

    third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -172,9 +172,9 @@ const NavMenu = styled.div`
       }
     }
   }
-  /* @media (max-width: 768px) {
+   @media (max-width: 768px) {
     display: none;
-  } */
+  } 
 `;
 
 const Login = styled.a`
